3493d650e8a8d77a58f410f5487589968dd743ef
Prathamesh <prathameshaj2004@gmail.com>
2025-01-23 18:11:05 +0530

    DSA

--- Stacks and Queues/Stacks/4-Infix to prefix conversion/Infixtoprefixconversion.cpp
+++ Stacks and Queues/Stacks/4-Infix to prefix conversion/Infixtoprefixconversion.cpp
@@ -17,52 +17,6 @@ bool isOperator(char c) {
     return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
 }
 
-// Function to convert infix expression to postfix
-string infixToPostfix(string infix) {
-    stack<char> st;  // Stack to hold operators and parentheses
-    string postfix = "";  // Resultant postfix expression
-
-    // Loop through each character in the infix expression
-    for (int i = 0; i < infix.length(); i++) {
-        char ch = infix[i];
-
-        // If the character is an operand, add it to the postfix expression
-        if (isalnum(ch)) {
-            postfix += ch;
-        }
-        // If the character is an opening parenthesis, push it to the stack
-        else if (ch == '(') {
-            st.push(ch);
-        }
-        // If the character is a closing parenthesis
-        else if (ch == ')') {
-            // Pop from the stack until an opening parenthesis is found
-            while (!st.empty() && st.top() != '(') {
-                postfix += st.top();
-                st.pop();
-            }
-            st.pop(); // Remove the opening parenthesis
-        }
-        // If the character is an operator
-        else if (isOperator(ch)) {
-            // Pop operators from the stack with greater or equal precedence
-            while (!st.empty() && precedence(st.top()) >= precedence(ch)) {
-                postfix += st.top();
-                st.pop();
-            }
-            st.push(ch); // Push the current operator to the stack
-        }
-    }
-
-    // Pop any remaining operators from the stack
-    while (!st.empty()) {
-        postfix += st.top();
-        st.pop();
-    }
-
-    return postfix;
-}
-
 // Function to reverse a string
 string reverseString(string str) {
     reverse(str.begin(), str.end());
@@ -83,11 +37,49 @@ string infixToPrefix(string infix) {
         }
     }
 
-    // Convert the modified infix expression to postfix
-    string postfix = infixToPostfix(revInfix);
+    stack<char> st;
+    string prefix = "";
 
-    // Reverse the postfix expression to get the prefix expression
-    string prefix = reverseString(postfix);
+    // Loop through each character in the reversed infix expression
+    for (int i = 0; i < revInfix.length(); i++) {
+        char ch = revInfix[i];
+
+        // If the character is an operand, add it to the prefix expression
+        if (isalnum(ch)) {
+            prefix += ch;
+        }
+        // If the character is an opening parenthesis, push it to the stack
+        else if (ch == '(') {
+            st.push(ch);
+        }
+        // If the character is a closing parenthesis
+        else if (ch == ')') {
+            // Pop from the stack until an opening parenthesis is found
+            while (!st.empty() && st.top() != '(') {
+                prefix += st.top();
+                st.pop();
+            }
+            st.pop(); // Remove the opening parenthesis
+        }
+        // If the character is an operator
+        else if (isOperator(ch)) {
+            // Pop operators from the stack with greater or equal precedence
+            while (!st.empty() && precedence(st.top()) > precedence(ch)) {
+                prefix += st.top();
+                st.pop();
+            }
+            st.push(ch); // Push the current operator to the stack
+        }
+    }
+
+    // Pop any remaining operators from the stack
+    while (!st.empty()) {
+        prefix += st.top();
+        st.pop();
+    }
+
+    // Reverse the prefix expression to get the final prefix result
+    prefix = reverseString(prefix);
 
     return prefix;
 }
