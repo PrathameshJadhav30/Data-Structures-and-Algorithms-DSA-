6cd260935b714a4652903e5283359c69ea723fe3
Prathamesh <prathameshaj2004@gmail.com>
2025-02-03 20:59:04 +0530

    DSA

---
+++ Trees/1-Binary Trees/05-Preorder Tree Traversal without Recursion/PreorderTreeTraversalwithoutRecursion.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void preorderTraversal(TreeNode* root) {
+    if (!root) return;
+
+    stack<TreeNode*> st;
+    st.push(root);
+
+    while (!st.empty()) {
+        TreeNode* node = st.top();
+        st.pop();
+        cout << node->val << " ";
+
+        if (node->right) st.push(node->right);
+        if (node->left) st.push(node->left);
+    }
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    cout << "Preorder Traversal without Recursion: ";
+    preorderTraversal(root);
+
+    return 0;
+}
