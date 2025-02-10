08ecebbec2b1c366a69d7395cda13f077e1247e5
Prathamesh <prathameshaj2004@gmail.com>
2025-02-10 21:12:54 +0530

    DSA

---
+++ Trees/2- Binary Search Trees/03-BST Insertion in a Binary Search Tree/BSTInsertioninaBinarySearchTree.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+// Definition of a BST Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = right = nullptr;
+    }
+};
+
+// Function to insert a node in BST
+Node* insert(Node* root, int value) {
+    if (root == nullptr) {
+        return new Node(value);
+    }
+    
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+    
+    return root;
+}
+
+// Inorder Traversal (Left, Root, Right)
+void inorder(Node* root) {
+    if (root == nullptr) {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+    
+    cout << "Inorder traversal of BST: ";
+    inorder(root);
+    cout << endl;
+    
+    return 0;
+}
